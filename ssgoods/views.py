c6befd355871ada5a18cda7605388b56b37adbc6
Minhui Choe <mchoe@adobe.com>
2023-11-25 23:05:47 +0900

    fix ssgoods views

--- ssgoods/views.py
+++ ssgoods/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello Adobe Day!")
+
+def input_list(request):
+    return render(request, 'input_list.html',{})

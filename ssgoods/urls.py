03285e3577ad5eb7cb0249af351af626fd8edbee
Minhui Choe <mchoe@adobe.com>
2023-11-27 09:24:54 +0900

    static3

--- ssgoods/urls.py
+++ ssgoods/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.input_list, name='input_list'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('search/confirm/', views.confirm, name='confirm'),
     path('search/duplication/', views.duplication, name='duplication'),
     path('search/none_user/', views.none_user, name='none_user')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

1b99323ca7a27a13f20eb231507d378701d2c39d
Minhui Choe <mchoe@adobe.com>
2023-11-28 01:29:51 +0900

    fix10

--- goods_adobeday_adobe/settings.py
+++ goods_adobeday_adobe/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.env
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
 # STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")   

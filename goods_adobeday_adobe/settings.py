d2d6111d3c5a7da76110f7b45fe748e95c21bf41
Minhui Choe <mchoe@adobe.com>
2023-11-27 11:41:10 +0900

    delete

--- goods_adobeday_adobe/settings.py
+++ goods_adobeday_adobe/settings.py
@@ -13,8 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -66,7 +66,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'ssgoods.context_processors.Confirm_Context'
             ],
         },
     },
@@ -120,13 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / "static",]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static",]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
-STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")   
 
-STATICFILES_DIRS = []
+# STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
+# STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")   
+
+# STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

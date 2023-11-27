832dc9d85d96f715ad1a9f3f24653bc43cb18152
Minhui Choe <mchoe@adobe.com>
2023-11-28 00:34:19 +0900

    fix4

--- goods_adobeday_adobe/settings.py
+++ goods_adobeday_adobe/settings.py
@@ -13,8 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,9 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),
-                    os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
 # STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")   
